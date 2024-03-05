2f4922ef3e90967bd94f23b9d766a6ebd0d981e1
Anton Protsenko <protseng@mail.ru>
2024-03-06 00:13:43 +0400

    Обработчики исключений + Кастомный класс исключения

---
+++ src/main/java/com/protsenko/bankdemo/exception/CustomExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.protsenko.bankdemo.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class CustomExceptionHandler
+{
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> constraintViolation(RuntimeException err)
+    {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HashMap.SimpleImmutableEntry<String,String>("error_message",err.getMessage()));
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity<?> notFoundUser(RuntimeException err)
+    {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new HashMap.SimpleImmutableEntry<String,String>("error_message",err.getMessage()));
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<?> notFoundEntity(RuntimeException er)
+    {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HashMap.SimpleImmutableEntry<String,String>("error_message",er.getMessage()));
+    }
+
+    @ExceptionHandler({HttpCustomException.class})
+    public ResponseEntity<?> catchCustomException(HttpCustomException err)
+    {
+        return ResponseEntity.status(err.getHttpStatus()).body(new HashMap.SimpleImmutableEntry<String,String>("error_message",err.getMessage()));
+    }
+}
---
+++ src/main/java/com/protsenko/bankdemo/exception/HttpCustomException.java
@@ -0,0 +1,42 @@
+package com.protsenko.bankdemo.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class HttpCustomException extends RuntimeException
+{
+    protected HttpStatus httpStatus;
+    protected String message;
+    public HttpCustomException(HttpStatus httpStatus)
+    {
+        this.httpStatus = httpStatus;
+    }
+    public HttpCustomException(HttpStatus httpStatus, String message)
+    {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpCustomException(HttpStatus httpStatus, BindingResult bindingResult)
+    {
+        this(httpStatus);
+        this.message = convertBindingResultToMap(bindingResult).toString();
+    }
+
+    private String convertBindingResultToMap(BindingResult bindingResult)
+    {
+        Map<String, String> errors = new HashMap<>();
+        bindingResult.getAllErrors().stream().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return errors.toString();
+    }
+}
